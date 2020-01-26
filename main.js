064379692d5b49de337c4cde95d41907a0387dd6
tommyfoster <32079533+tommyfoster@users.noreply.github.com>
2020-01-26 17:01:54 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿jQuery(document).ready(function ($) {
+    $('.modifycomment').on('click', function () {
+        var commentid = $(this).attr("id").split('-')[1];
+        $('#modifycommentid').val(commentid);
+        $('#modifycommenttext').val($('#commentnew-' + commentid).val());
+    });
+});
+
